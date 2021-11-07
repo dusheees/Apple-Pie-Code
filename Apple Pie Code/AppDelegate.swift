e1ae05c94371cf09dabbd4fb2b4d5338e0aac10b
Andrey Zhilin <komplayer31@yandex.ru>
2021-11-08 02:26:27 +0900

    Created UI Programmatically

--- Apple Pie Code/AppDelegate.swift
+++ Apple Pie Code/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .red
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
         return true
